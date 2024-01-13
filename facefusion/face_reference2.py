f5f0f1eff4cd40d6a5117af8c2d83e2911376ca5
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-01-13 19:43:27 -0300

    Update face_reference2.py

--- facefusion/face_reference2.py
+++ facefusion/face_reference2.py
@@ -5,17 +5,17 @@ from facefusion.typing import Face
 FACE_REFERENCE2 = None
 
 
-def get_face_reference() -> Optional[Face]:
+def get_face_reference2() -> Optional[Face]:
 	return FACE_REFERENCE2
 
 
-def set_face_reference(face : Face) -> None:
+def set_face_reference2(face : Face) -> None:
 	global FACE_REFERENCE2
 
 	FACE_REFERENCE2 = face
 
 
-def clear_face_reference() -> None:
+def clear_face_reference2() -> None:
 	global FACE_REFERENCE2
 
 	FACE_REFERENCE2 = None
