7b8351974747afae22d9a490eb46828e91e33f61
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-01-13 20:49:26 -0300

    Update face_selector.py

--- facefusion/uis/components/face_selector.py
+++ facefusion/uis/components/face_selector.py
@@ -136,10 +136,12 @@ def clear_and_update_reference_face_position2(event : gradio.SelectData) -> grad
 
 def update_reference_face_position(reference_face_position : int = 0) -> None:
 	print('Alterou face reference position 1')
+	print(reference_face_position)
 	facefusion.globals.reference_face_position = reference_face_position
 
 def update_reference_face_position2(reference_face_position2 : int = 0) -> None:
 	print('Alterou face reference position 2')
+	print(reference_face_position2)
 	facefusion.globals.reference_face_position2 = reference_face_position2
 
 def update_reference_face_distance(reference_face_distance : float) -> None:
