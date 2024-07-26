02ff62270a03869641e330c507d1f99008c748dd
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-07-26 18:01:42 -0300

    tudo

---
+++ facefusion/ffmpeg.py
@@ -0,0 +1,166 @@
+from typing import List
+import subprocess
+
+import facefusion.globals
+from facefusion import logger
+from facefusion.filesystem import get_temp_frames_pattern, get_temp_output_video_path, get_temp_directory_path
+from facefusion.vision import detect_fps, count_video_frame_total
+import ffmpeg
+from tqdm import tqdm
+import os
+
+
+def run_ffmpeg(input_command : List[str], desc="Processando video", tamanho=0):
+    frames = tamanho
+    commands = [ 'ffmpeg']
+    commands.extend(input_command)
+    # Cria um objeto tqdm para exibir a barra de progresso
+    with tqdm(total=frames, desc=desc, position=0, leave=True) as progress:
+        # Abre um processo para executar o comando ffmpeg
+        process = subprocess.Popen(
+            commands,
+            stderr=subprocess.PIPE,  # Captura a saída de erro do ffmpeg
+            universal_newlines=True,  # Usa strings para texto (em vez de bytes)
+            bufsize=1,  # Buffer de linha a linha
+        )
+
+        # Lê a saída de erro do processo linha por linha
+        for line in process.stderr:
+            # Aqui você precisaria analisar a saída do ffmpeg para extrair informações de progresso
+            # e atualizar a barra de progresso. Isso depende do formato específico de saída do ffmpeg.
+
+            # Exemplo: Verifica se a linha contém informações de progresso
+            if "frame=" in line and "fps=" in line:
+                # Extraia o número do frame atual e atualize a barra de progresso
+                current_frame = int(line.split("frame=")[1].split()[0])
+                progress.update(current_frame - progress.n)
+
+        # Aguarde o término do processo
+        process.wait()
+
+    # Verifica o código de saída do processo
+    if process.returncode != 0:
+        print(f"Erro ao executar o comando: {input_command}")
+
+    return True
+
+def run_ffmpeg2(args : List[str]) -> bool:
+	commands = [ 'ffmpeg', '-hide_banner', '-loglevel', 'error' ]
+	commands.extend(args)
+	try:
+		subprocess.run(commands, stderr = subprocess.PIPE, check = True)
+		return True
+	except subprocess.CalledProcessError as exception:
+		logger.debug(exception.stderr.decode().strip(), __name__.upper())
+		return False
+
+
+def open_ffmpeg(args : List[str]) -> subprocess.Popen[bytes]:
+	commands = [ 'ffmpeg', '-hide_banner', '-loglevel', 'error' ]
+	commands.extend(args)
+	return subprocess.Popen(commands, stdin = subprocess.PIPE)
+
+
+def extract_frames(target_path : str, fps : float) -> bool:
+	temp_frame_compression = round(31 - (facefusion.globals.temp_frame_quality * 0.31))
+	trim_frame_start = facefusion.globals.trim_frame_start
+	trim_frame_end = facefusion.globals.trim_frame_end
+	temp_frames_pattern = get_temp_frames_pattern(target_path, '%04d')
+	commands = [ '-hwaccel', 'auto', '-i', target_path, '-q:v', str(temp_frame_compression), '-pix_fmt', 'rgb24' ]
+	if trim_frame_start is not None and trim_frame_end is not None:
+		commands.extend([ '-vf', 'trim=start_frame=' + str(trim_frame_start) + ':end_frame=' + str(trim_frame_end) + ',fps=' + str(fps) ])
+	elif trim_frame_start is not None:
+		commands.extend([ '-vf', 'trim=start_frame=' + str(trim_frame_start) + ',fps=' + str(fps) ])
+	elif trim_frame_end is not None:
+		commands.extend([ '-vf', 'trim=end_frame=' + str(trim_frame_end) + ',fps=' + str(fps) ])
+	else:
+		commands.extend([ '-vf', 'fps=' + str(fps) ])
+	commands.extend([ '-vsync', '0', temp_frames_pattern ])
+	commands.extend([ '-c:v', 'h264_cuvid ' ])
+	return run_ffmpeg(commands, "Extraindo frames", len(os.listdir(get_temp_directory_path(target_path))))
+
+
+def compress_image(output_path : str) -> bool:
+	output_image_compression = round(31 - (facefusion.globals.output_image_quality * 0.31))
+	commands = [ '-hwaccel', 'auto', '-i', output_path, '-q:v', str(output_image_compression), '-y', output_path ]
+	return run_ffmpeg2(commands)
+
+
+def merge_video(target_path : str, fps : float) -> bool:
+	renomear_frames(get_temp_directory_path(target_path))
+	print(len(os.listdir(get_temp_directory_path(target_path))))
+	print(get_temp_directory_path(target_path))
+	temp_output_video_path = get_temp_output_video_path(target_path)
+	temp_frames_pattern = get_temp_frames_pattern(target_path, '%04d')
+	commands = [ '-hwaccel', 'auto', '-r', str(fps), '-i', temp_frames_pattern, '-c:v', facefusion.globals.output_video_encoder ]
+	if facefusion.globals.output_video_encoder in [ 'libx264', 'libx265' ]:
+		output_video_compression = round(51 - (facefusion.globals.output_video_quality * 0.51))
+		commands.extend([ '-crf', str(output_video_compression) ])
+	if facefusion.globals.output_video_encoder in [ 'libvpx-vp9' ]:
+		output_video_compression = round(63 - (facefusion.globals.output_video_quality * 0.63))
+		commands.extend([ '-crf', str(output_video_compression) ])
+	if facefusion.globals.output_video_encoder in [ 'h264_nvenc', 'hevc_nvenc' ]:
+		output_video_compression = round(51 - (facefusion.globals.output_video_quality * 0.51))
+		commands.extend([ '-cq', str(output_video_compression) ])
+	commands.extend([ '-pix_fmt', 'yuv420p', '-colorspace', 'bt709', '-y', temp_output_video_path ])
+	return run_ffmpeg(commands, "Criando o vídeo", len(os.listdir(get_temp_directory_path(target_path))))
+
+def get_crf_value(quality):
+    if facefusion.globals.output_video_encoder in ['libx264', 'libx265']:
+        return round(51 - (quality * 0.51))
+    elif facefusion.globals.output_video_encoder in ['libvpx-vp9']:
+        return round(63 - (quality * 0.63))
+    elif facefusion.globals.output_video_encoder in ['h264_nvenc', 'hevc_nvenc']:
+        return round(51 - (quality * 0.51))
+    else:
+        return None  # Add handling for other cases as needed
+
+def merge_video2(target_path: str, fps: float) -> bool:
+    temp_output_video_path = get_temp_output_video_path(target_path)
+    temp_frames_pattern = get_temp_frames_pattern(target_path, '%04d')
+
+    ffmpeg.input(temp_frames_pattern, r=fps, hwaccel='cuda').output(
+        temp_output_video_path,
+        vcodec=facefusion.globals.output_video_encoder,
+        crf=get_crf_value(facefusion.globals.output_video_quality),
+        pix_fmt='yuv420p',
+        colorspace='bt709',
+        y='-y'
+    ).run()
+
+    return True  # Replace with appropriate logic for success/failure
+
+
+def restore_audio(target_path : str, output_path : str) -> bool:
+	fps = detect_fps(target_path)
+	trim_frame_start = facefusion.globals.trim_frame_start
+	trim_frame_end = facefusion.globals.trim_frame_end
+	temp_output_video_path = get_temp_output_video_path(target_path)
+	commands = [ '-hwaccel', 'auto', '-i', temp_output_video_path ]
+	if trim_frame_start is not None:
+		start_time = trim_frame_start / fps
+		commands.extend([ '-ss', str(start_time) ])
+	if trim_frame_end is not None:
+		end_time = trim_frame_end / fps
+		commands.extend([ '-to', str(end_time) ])
+	commands.extend([ '-i', target_path, '-c', 'copy', '-map', '0:v:0', '-map', '1:a:0', '-shortest', '-y', output_path ])
+	return run_ffmpeg2(commands)
+
+def renomear_frames(pasta_frames):
+	# Lista dos arquivos na pasta de frames
+	arquivos = os.listdir(pasta_frames)
+	
+	# Filtra apenas os arquivos de imagem (JPEG)
+	arquivos_imagem = [arquivo for arquivo in arquivos if arquivo.lower().endswith(('.jpg', '.jpeg'))]
+	
+	# Ordena os arquivos por nome
+	arquivos_imagem.sort()
+	
+	# Renomeia os arquivos na ordem numérica sequencial
+	for idx, arquivo in enumerate(arquivos_imagem, start=1):
+		novo_nome = f'{idx:04d}.jpg'  # Define o novo nome do arquivo
+		caminho_original = os.path.join(pasta_frames, arquivo)
+		caminho_novo = os.path.join(pasta_frames, novo_nome)
+		os.rename(caminho_original, caminho_novo)  # Renomeia o arquivo
+	
+	print("Frames renomeados com sucesso.")
--- facefusion/installer.py
+++ facefusion/installer.py
@@ -1,42 +1,55 @@
 from typing import Dict, Tuple
+import sys
+import os
+import platform
+import tempfile
 import subprocess
-from argparse import ArgumentParser, HelpFormatter
-
-subprocess.call([ 'pip', 'install' , 'inquirer', '-q' ])
-
 import inquirer
+from argparse import ArgumentParser, HelpFormatter
 
 from facefusion import metadata, wording
 
 TORCH : Dict[str, str] =\
 {
 	'default': 'default',
-	'cpu': 'cpu',
-	'cuda': 'cu118',
-	'rocm': 'rocm5.6'
+	'cpu': 'cpu'
 }
-ONNXRUNTIMES : Dict[str, Tuple[str, str]] =\
-{
-	'default': ('onnxruntime', '1.15.1'),
-	'cuda': ('onnxruntime-gpu', '1.15.1'),
-	'coreml-legacy': ('onnxruntime-coreml', '1.13.1'),
-	'coreml-silicon': ('onnxruntime-silicon', '1.16.0'),
-	'directml': ('onnxruntime-directml', '1.16.3'),
-	'openvino': ('onnxruntime-openvino', '1.16.0')
-}
+ONNXRUNTIMES : Dict[str, Tuple[str, str]] = {}
+
+if platform.system().lower() == 'darwin':
+	ONNXRUNTIMES['default'] = ('onnxruntime', '1.17.0')
+else:
+	ONNXRUNTIMES['default'] = ('onnxruntime', '1.16.3')
+if platform.system().lower() == 'linux' or platform.system().lower() == 'windows':
+	TORCH['cuda-12.1'] = 'cu121'
+	TORCH['cuda-11.8'] = 'cu118'
+	ONNXRUNTIMES['cuda-12.1'] = ('onnxruntime-gpu', '1.17.0')
+	ONNXRUNTIMES['cuda-11.8'] = ('onnxruntime-gpu', '1.16.3')
+	ONNXRUNTIMES['openvino'] = ('onnxruntime-openvino', '1.16.0')
+if platform.system().lower() == 'linux':
+	TORCH['rocm-5.4.2'] = 'rocm5.4.2'
+	TORCH['rocm-5.6'] = 'rocm5.6'
+	ONNXRUNTIMES['rocm-5.4.2'] = ('onnxruntime-rocm', '1.16.3')
+	ONNXRUNTIMES['rocm-5.6'] = ('onnxruntime-rocm', '1.16.3')
+if platform.system().lower() == 'windows':
+	ONNXRUNTIMES['directml'] = ('onnxruntime-directml', '1.16.0')
 
 
 def cli() -> None:
-	program = ArgumentParser(formatter_class = lambda prog: HelpFormatter(prog, max_help_position = 120))
-	program.add_argument('--torch', help = wording.get('install_dependency_help').format(dependency = 'torch'), dest = 'torch', choices = TORCH.keys())
-	program.add_argument('--onnxruntime', help = wording.get('install_dependency_help').format(dependency = 'onnxruntime'), dest = 'onnxruntime', choices = ONNXRUNTIMES.keys())
+	program = ArgumentParser(formatter_class = lambda prog: HelpFormatter(prog, max_help_position = 130))
+	program.add_argument('--torch', help = wording.get('help.install_dependency').format(dependency = 'torch'), choices = TORCH.keys())
+	program.add_argument('--onnxruntime', help = wording.get('help.install_dependency').format(dependency = 'onnxruntime'), choices = ONNXRUNTIMES.keys())
+	program.add_argument('--skip-venv', help = wording.get('help.skip_venv'), action = 'store_true')
 	program.add_argument('-v', '--version', version = metadata.get('name') + ' ' + metadata.get('version'), action = 'version')
 	run(program)
 
 
 def run(program : ArgumentParser) -> None:
 	args = program.parse_args()
+	python_id = 'cp' + str(sys.version_info.major) + str(sys.version_info.minor)
 
+	if not args.skip_venv:
+		os.environ['PIP_REQUIRE_VIRTUALENV'] = '1'
 	if args.torch and args.onnxruntime:
 		answers =\
 		{
@@ -46,18 +59,34 @@ def run(program : ArgumentParser) -> None:
 	else:
 		answers = inquirer.prompt(
 		[
-			inquirer.List('torch', message = wording.get('install_dependency_help').format(dependency = 'torch'), choices = list(TORCH.keys())),
-			inquirer.List('onnxruntime', message = wording.get('install_dependency_help').format(dependency = 'onnxruntime'), choices = list(ONNXRUNTIMES.keys()))
+			inquirer.List('torch', message = wording.get('help.install_dependency').format(dependency = 'torch'), choices = list(TORCH.keys())),
+			inquirer.List('onnxruntime', message = wording.get('help.install_dependency').format(dependency = 'onnxruntime'), choices = list(ONNXRUNTIMES.keys()))
 		])
 	if answers:
 		torch = answers['torch']
 		torch_wheel = TORCH[torch]
-		onnxruntime = answers['onnxruntime']
-		onnxruntime_name, onnxruntime_version = ONNXRUNTIMES[onnxruntime]
-		subprocess.call([ 'pip', 'uninstall', 'torch', '-y' ])
+		onnxruntime = 'cuda-12.1'
+		onnxruntime_name, onnxruntime_version = ('onnxruntime-gpu', '1.17.0')
+
+		subprocess.call([ 'pip', 'uninstall', 'torch', '-y', '-q' ])
 		if torch_wheel == 'default':
-			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt' ])
+			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--force-reinstall' ])
 		else:
-			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', 'https://download.pytorch.org/whl/' + torch_wheel ])
-		subprocess.call([ 'pip', 'uninstall', 'onnxruntime', onnxruntime_name, '-y' ])
-		subprocess.call([ 'pip', 'install', onnxruntime_name + '==' + onnxruntime_version ])
+			subprocess.call([ 'pip', 'install', '-r', 'requirements.txt', '--extra-index-url', 'https://download.pytorch.org/whl/cu121', '--force-reinstall' ])
+		if onnxruntime == 'rocm-5.4.2' or onnxruntime == 'rocm-5.6':
+			if python_id in [ 'cp39', 'cp310', 'cp311' ]:
+				rocm_version = onnxruntime.replace('-', '')
+				rocm_version = rocm_version.replace('.', '')
+				wheel_name = 'onnxruntime_training-' + onnxruntime_version + '+' + rocm_version + '-' + python_id + '-' + python_id + '-manylinux_2_17_x86_64.manylinux2014_x86_64.whl'
+				wheel_path = os.path.join(tempfile.gettempdir(), wheel_name)
+				wheel_url = 'https://download.onnxruntime.ai/' + wheel_name
+				subprocess.call([ 'curl', '--silent', '--location', '--continue-at', '-', '--output', wheel_path, wheel_url ])
+				subprocess.call([ 'pip', 'uninstall', wheel_path, '-y', '-q' ])
+				subprocess.call([ 'pip', 'install', wheel_path, '--force-reinstall' ])
+				os.remove(wheel_path)
+		else:
+			subprocess.call([ 'pip', 'uninstall', 'onnxruntime', 'onnxruntime-gpu', '-y', '-q' ])
+			if onnxruntime == 'cuda-12.1':
+				subprocess.call([ 'pip install onnxruntime-gpu==1.17.0 --extra-index-url https://pkgs.dev.azure.com/onnxruntime/onnxruntime/_packaging/onnxruntime-cuda-12/pypi/simple --force-reinstall' ])
+			else:
+				subprocess.call([ 'pip', 'install', onnxruntime_name + '==' + onnxruntime_version, '--force-reinstall' ])
